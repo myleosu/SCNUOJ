3a03607c1165a2065894b5b27b3f24d47253c6d9
myleosu <1063371833@qq.com>
2019-10-11 00:14:06 +0800

    frist commit

---
+++ submission/views/admin.py
@@ -0,0 +1,21 @@
+from account.decorators import super_admin_required
+from judge.tasks import judge_task
+# from judge.dispatcher import JudgeDispatcher
+from utils.api import APIView
+from ..models import Submission
+
+
+class SubmissionRejudgeAPI(APIView):
+    @super_admin_required
+    def get(self, request):
+        id = request.GET.get("id")
+        if not id:
+            return self.error("Parameter error, id is required")
+        try:
+            submission = Submission.objects.select_related("problem").get(id=id, contest_id__isnull=True)//将contest_id_isnull去掉即可重盘比赛题目
+        except Submission.DoesNotExist:
+            return self.error("Submission does not exists")
+        submission.statistic_info = {}
+        submission.save()
+        judge_task.send(submission.id, submission.problem.id)
+        return self.success()
